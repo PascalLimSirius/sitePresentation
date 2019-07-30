b2051e975bbf108c4203e936b0f9cd6ec8d63b8e
Pascal-Lim <pascallim1@gmail.com>
2019-07-30 10:47:36 +0200

    MajConfig + CarréOk

--- wordpress/wp-config-sample.php
+++ wordpress/wp-config-sample.php
@@ -84,12 +84,52 @@ $table_prefix = 'wp_';
  */
 define('WP_DEBUG', false);
 
-if( WP_DEBUG){
-  define ('WP_DEBUG_LOG', true);
-  define ('WP_DEBUG_DISPLAY', true);
+/*
+Sachant que WordPress ne nous permet pas de gérer l'environnement dans lequel est exécuté notre WordPress, nous mettons la fonctionnalités en place nous-même en créant une constante qui n'est comprise que pas notre code.
+*/
+// define( 'ENVIRONMENT', 'development' );
+// define( 'ENVIRONMENT', 'staging' );
+define( 'ENVIRONMENT', 'production' );
+/**
+ * Additionnal configuration constants
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php
+ */
+if ( defined( 'ENVIRONMENT' ) ) { // Je vérifie que la constante ENVIRONMENT existe
+    if ( ENVIRONMENT === 'development' ) {
+        define( 'WP_DEBUG_DISPLAY', true ); // Affichage des erreurs PHP/WordPress directement dans le code HTML
+        define( 'WP_DEBUG_LOG', false );
+        define( 'DISALLOW_FILE_MODS', false ); // Activation de l'ajout des fichiers de traduction, thèmes et plugins
+        define( 'FS_METHOD', 'direct' ); // Activation du téléchargement direct des fichiers de traduction, plugins et thèmes
+        define( 'WP_POST_REVISIONS', false ); // Désactivation du sytème de révions des contenus
+        define( 'SCRIPT_DEBUG', true ); // Activation du débogage des CSS et JS de WordPress
+        define( 'EMPTY_TRASH_DAYS', 0 ); // Désactivation de la corbeille
+    } elseif ( ENVIRONMENT === 'staging' ) {
+        define( 'WP_DEBUG_DISPLAY', false );
+        define( 'WP_DEBUG_LOG', true ); // Ecriture des erreurs PHP/WordPress dans un fichier de log
+        define( 'DISALLOW_FILE_MODS', true ); // Désactivation de l'ajout et des mises à jour des thèmes et plugins dans le backoffice
+        define( 'WP_POST_REVISIONS', 10 ); // Limitation du nombre de révions des contenus
+        define( 'SCRIPT_DEBUG', false );
+        define( 'EMPTY_TRASH_DAYS', 60 ); // Limitation du nombre de jours de conservation des contenus dans la corbeille
+    } elseif ( ENVIRONMENT === 'production' ) {
+        define( 'WP_DEBUG_DISPLAY', false );
+        define( 'WP_DEBUG_LOG', true );
+        define( 'DISALLOW_FILE_MODS', true );
+        define( 'WP_POST_REVISIONS', 10 );
+        define( 'SCRIPT_DEBUG', false );
+        define( 'EMPTY_TRASH_DAYS', 60 );
+    } else {
+        echo 'La valeur de la constante ENVIRONMENT n\'est pas valide. Les valeurs possibles sont development, staging ou production.';
+        exit;
+    }
+} else {
+    echo 'La constante ENVIRONMENT n\'est pas définie.';
+    exit;
 }
-
-define('DISALLOW_FILE_EDIT',true);
+define( 'DISALLOW_FILE_EDIT', true ); // Désactivation de l'éditeur embarqué de thèmes et plugins
+define( 'AUTOMATIC_UPDATER_DISABLED', true ); // Désactivation des mises à jour automatiques de WordPress
+define( 'WP_AUTO_UPDATE_CORE', false ); // Désactivation des mises à jour du cœur de WordPress
+/* That's all, stop editing! Happy publishing. */
 
 /* C’est tout, ne touchez pas à ce qui suit ! Bonne publication. */
 
@@ -99,8 +139,4 @@ if ( !defined('ABSPATH') )
 
 /** Réglage des variables de WordPress et de ses fichiers inclus. */
 require_once(ABSPATH . 'wp-settings.php');
-
-define('FS_METHOD','direct');
-
-define('DISALLOW_FILE_MODS', true);
 
